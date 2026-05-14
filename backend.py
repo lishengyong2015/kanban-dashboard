6a48ebc545aad0551055de3c3c13467303498e7d
lishengyong <lishengyong2015@163.com>
2026-05-15 07:42:54 +0800

    fix: 键盘导航选中高亮bug、端口改为50000、增加priority标签、详情页显示依赖

--- backend.py
+++ backend.py
@@ -367,4 +367,4 @@ def manage_link(data: dict = {}):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=50000)
